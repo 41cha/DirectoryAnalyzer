970bd38d0b78739222f13e6833253cf8f91b6987
Vova <vovaa.dzimina@gmail.com>
2026-01-24 21:39:11 +0200

    add gitignore

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="directory_analyzer",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     author="Volodymyr",
     author_email="vova.dzimina@gmail.com",
